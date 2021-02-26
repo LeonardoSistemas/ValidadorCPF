3a11c669309cc82e2bf76fa7da7112d98df13a6b
Leonardo Treviso <leonardosistemas@outlook.com>
2021-02-26 20:16:11 -0300

    Criei a função para passar a string do cpf para o array

--- validador-cpf.js
+++ validador-cpf.js
@@ -3,4 +3,13 @@ let arraycomcalculoprimeirodigito = []
 
 function capturarcpfdigitado(){
     let cpfdigitado = document.getElementById('txtNumero').value
+    montararrayprimeirodigito(cpfdigitado)
+}
+
+function montararrayprimeirodigito(cpfdigitado){
+    let cpfdigitadoformatado = cpfdigitado.substring(0,9)
+
+    for(let indexfor = 0; indexfor < cpfdigitadoformatado.length; indexfor++){
+        arraycpfdigitado.push(cpfdigitadoformatado.substring(indexfor, indexfor + 1))
+    }
 }
